227d879bd42454b65b4165ed6b9a7f93e11b1aec
user-1 <user-1@MacBook-Pro.local>
2020-08-08 19:43:11 -0600

    added ternary to display default product image

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -4,9 +4,13 @@ const Product = (props) => {
     const {name, price, imgurl} = props.product;
     return(
         <div className="product-box">
-            <div className="product-img">
+            <div className="product-img"> {!imgurl ? (
                 <img className="product-img" 
-                src={imgurl} alt="https://s3.amazonaws.com/bloc-global-assets/almanac-assets/bootcamps/logos/000/002/656/original/DevMountain.jpg?1467187319"/>
+                src="https://s3.amazonaws.com/bloc-global-assets/almanac-assets/bootcamps/logos/000/002/656/original/DevMountain.jpg?1467187319" alt="Product Image"/>
+            ) : (
+                <img className="product-img" 
+                src={imgurl} alt="DevMtn Logo"/>        
+        )}
             </div>
             <div className="product-info">
                 <h3 className="name">{name}</h3>
@@ -19,3 +23,4 @@ const Product = (props) => {
 }
 
 export default Product;
+
